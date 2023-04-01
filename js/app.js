4fb3a3d8a2e8be47bde588173c931d26d1880905
nazarisabbot <n.updesign@gmail.com>
2023-04-02 00:26:22 +0600

    fix mistake #1  hm 4:1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,56 @@
-const nameProporty = document.querySelectorAll(".board__name");
+"use strict";
 
-for (let i = 0; i < nameProporty.length; i++) {
-  console.log(nameProporty[i].textContent);
+function logTable(obj) {
+  let res = [];
+  res[0] = obj.name;
+  res[1] = obj.quantity;
+  res[2] = obj.price;
+  res[3] = obj.total;
+  return res;
 }
+
+const firstProporty = {
+  id: 246016548,
+  name: "Стиральная машина LG",
+  quantity: 5,
+  price: 1500,
+  get total() {
+    return this.quantity * this.price;
+  },
+  ttt: this.total,
+};
+
+const secondProporty = {
+  id: 231548925,
+  name: "Телевизор Samsung",
+  quantity: 2,
+  price: 1250,
+  get total() {
+    return this.quantity * this.price;
+  },
+};
+
+const thirdProporty = {
+  id: 2548965326,
+  name: "Холодильник LG",
+  quantity: 5,
+  price: 750,
+  get total() {
+    return this.quantity * this.price;
+  },
+};
+
+const fourthProporty = {
+  id: 856478257,
+  name: "Морозильная камера Gorenje",
+  quantity: 9,
+  price: 850,
+  get total() {
+    return this.quantity * this.price;
+  },
+};
+
+console.table(logTable(firstProporty));
+console.table(logTable(secondProporty));
+console.table(logTable(thirdProporty));
+console.table(logTable(fourthProporty));
