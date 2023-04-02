9d3c66cc1b52311d60c4292628d2346f623d4eb9
nazarisabbot <n.updesign@gmail.com>
2023-04-03 00:28:48 +0600

    hm 4:2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//hw 4:1
 function logTable(obj) {
   let res = [];
   res[0] = obj.name;
@@ -54,3 +55,40 @@ console.table(logTable(firstProporty));
 console.table(logTable(secondProporty));
 console.table(logTable(thirdProporty));
 console.table(logTable(fourthProporty));
+
+//hw 4:2
+(function () {
+  const nameRequest = {
+    name: "Картофель",
+    category: "",
+    quantity: null,
+    price: null,
+
+    set requestName(value) {
+      this.name = String(value);
+    },
+    set requestСategory(value) {
+      this.category = String(value);
+    },
+    set requestQuantity(value) {
+      this.quantity = Number(value);
+    },
+    set requestPrice(value) {
+      this.price = Number(value);
+    },
+    get total() {
+      return this.quantity * this.price;
+    },
+  };
+
+  nameRequest.requestName = prompt("Укажите пожалуйста наименование товара");
+  nameRequest.requestСategory = prompt("Укажите пожалуйста категорию товара");
+  nameRequest.requestQuantity = prompt("Укажите пожалуйста количество товара");
+  nameRequest.requestPrice = prompt("Укажите пожалуйста стоимость товара");
+
+  console.table(nameRequest);
+
+  console.log(
+    `На складе ${nameRequest.quantity} единицы товара ${nameRequest.name} на сумму ${nameRequest.total} деревянных`
+  );
+})();
---
+++ js/einstein.js
@@ -0,0 +1,6 @@
+const myWeight = 120;
+const speedLight = 299792458;
+
+const res = myWeight * speedLight * 2;
+
+console.log(res);
