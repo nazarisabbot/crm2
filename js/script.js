5f11e57d5e6731a51ed39ea3944d49c36d082c69
nazarisabbot <n.updesign@gmail.com>
2023-05-04 23:50:56 +0600

    add close open modal window script // get elements of form

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/* Close error script */
+
+const closeError = document.getElementById("closeError");
+const blockError = document.querySelector(".form-card__error");
+
+closeError.addEventListener("click", () => {
+  blockError.style.display = "none";
+});
+
+/* Close / Open form card script */
+
+const closeForm = document.getElementById("closeForm");
+const openForm = document.getElementById("openForm");
+const blockForm = document.querySelector(".form");
+
+closeForm.addEventListener("click", () => {
+  blockForm.style.display = "none";
+});
+
+openForm.addEventListener("click", () => {
+  blockForm.style.display = "flex";
+});
+
+// Get Elements
+
+const titleAction = document.querySelector(".form-card__action");
+// Кнопка возле id, была определена выше называется closeError
+const idProduct = document.querySelector(".form-card__number");
+const formCard = document.querySelector(".form-card__form");
+const checkBox = document.querySelector(".form-card__checkbox");
+const inputCommitForDiscount = document.querySelector("#commit");
+const totalCart = document.querySelector("#totalCart");
