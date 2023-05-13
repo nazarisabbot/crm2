1356062c2d93731c6f4fcbeeec8b0072e806014b
nazarisabbot <n.updesign@gmail.com>
2023-05-13 20:48:07 +0600

    add form control

--- js/createRow.js
+++ js/createRow.js
@@ -63,7 +63,22 @@ const goods = [
   },
 ];
 
+const form = document.getElementById("form");
+const commitArea = document.querySelector(".form-card__input_commit");
+const buttonAddFile = document.querySelector(".form-card__add");
+const previewImage = document.querySelector(".form-card__product");
 const list = document.querySelector(".board__body");
+const windowTotalMount = document.querySelector("#totalCart");
+const innerTotal = document.querySelector(".form-card_innertotal");
+
+const totalMountGoods = (arr) => {
+  const resArr = arr.reduce((acc, item) => {
+    return acc + item.price * item.count;
+  }, 0);
+  return resArr;
+};
+
+windowTotalMount.textContent = `$ ${totalMountGoods(goods).toLocaleString()}`;
 
 const createRow = ({ id, title, category, units, count, price }) => {
   const tr = document.createElement("tr");
@@ -105,13 +120,116 @@ list.addEventListener("click", (e) => {
     const elemForDel = e.target.closest(".board__list");
     const idForDel = +elemForDel.firstElementChild.textContent;
 
-    for (let i = 0; i < goods.length - 1; i++) {
+    for (let i = 0; i < goods.length; i++) {
       if (goods[i].id === idForDel) {
         goods.splice(i, 1);
       }
     }
 
+    windowTotalMount.textContent = `$ ${totalMountGoods(
+      goods
+    ).toLocaleString()}`;
+
     elemForDel.remove();
+
     console.log(goods);
   }
+});
+
+// Form control
+
+form.discount.addEventListener("change", () => {
+  if (form.discount.checked === false) {
+    commitArea.setAttribute("disabled", "disabled");
+  }
+
+  if (form.discount.checked === true) {
+    commitArea.removeAttribute("disabled");
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const objForm = Object.fromEntries(formData);
+
+  const {
+    category,
+    commit: discont,
+    count,
+    description,
+    name: title,
+    measurements: units,
+    price,
+    file,
+  } = objForm;
+
+  const newObj = {
+    id: Date.now(),
+    title,
+    price,
+    description,
+    category,
+    discont,
+    count,
+    units,
+    images: {
+      small: file.name,
+      big: file.name,
+    },
+  };
+
+  goods.push(newObj);
+
+  renderGoods([newObj]);
+
+  windowTotalMount.textContent = `$ ${totalMountGoods(goods).toLocaleString()}`;
+
+  form.reset();
+
+  blockForm.classList.remove("form_open");
+});
+
+const objEvent = {
+  priceMount: 0,
+  countMount: 0,
+  resMount: 0,
+  handleEvent(e) {
+    if (e.target.closest(".form-card__input_price")) {
+      this.priceMount = e.target.value;
+    }
+    if (e.target.closest(".form-card__input_count")) {
+      this.countMount = e.target.value;
+    }
+    this.resMount = Number(this.priceMount) * Number(this.countMount);
+    innerTotal.textContent = `$ ${this.resMount.toLocaleString()}`;
+  },
+};
+
+form.addEventListener("change", objEvent);
+
+buttonAddFile.addEventListener("change", () => {
+  const errorSize = document.querySelector(".form-card__red");
+  const buttonDelNewImg = document.querySelector(".form-card__delete");
+  const inputImg = document.querySelector(".form-card__image");
+
+  const pathNewImg = URL.createObjectURL(file.files[0]);
+  const sizeNewImg = file.files[0].size;
+
+  if (sizeNewImg >= 100000) {
+    errorSize.classList.add("form-card__red_active");
+  } else {
+    errorSize.classList.remove("form-card__red_active");
+  }
+
+  if (sizeNewImg <= 100000) {
+    inputImg.setAttribute("src", pathNewImg);
+  }
+
+  previewImage.classList.add("active");
+
+  buttonDelNewImg.addEventListener("click", () => {
+    inputImg.removeAttribute("src");
+  });
 });
--- js/script.js
+++ js/script.js
@@ -29,7 +29,6 @@ openForm.addEventListener("click", () => {
 // Get Elements
 
 const titleAction = document.querySelector(".form-card__action");
-// Кнопка возле id, была определена выше называется closeError
 const idProduct = document.querySelector(".form-card__number");
 const formCard = document.querySelector(".form-card__form");
 const checkBox = document.querySelector(".form-card__checkbox");
